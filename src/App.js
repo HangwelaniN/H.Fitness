666459f40f86dfec2b5f98ee4b24d637342f4b06
HangwelaniN <hngwekhulu@gmail.com>
2024-02-14 15:06:40 +0200

    removed error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       return(
          <h1>Hello world</h1>
-       )
+       
     </div>
   );
 }
